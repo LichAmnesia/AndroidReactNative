f89ca5c0d2ec988801ba2e8047c757eed5bc30e2
Lich_Amnesia <alwaysxiaop@gmail.com>
2016-08-28 11:46:46 -0600

    using navigator

--- AProject/index.android.js
+++ AProject/index.android.js
@@ -11,7 +11,12 @@ import {
   Text,
   View,
   Image,
+  TextInput,
+  ListView,
+  Navigator
 } from 'react-native';
+
+import MyScene from './MyScene';
 
 class Greeting extends Component {
   render() {
@@ -59,9 +64,110 @@ class AProject extends Component {
         <Greeting name='Jaina' />
         <Greeting name='Valeera' />
         <Blink  text='This is will blink every 3 seconds.'/>
+        <View style={{width: 50, height: 100, backgroundColor: 'powderblue'}}/>
       </View>
 
     );
+  }
+}
+
+
+class FlexDimensionsBasics extends Component {
+  render() {
+    return (
+      // Try removing the `flex: 1` on the parent View.
+      // The parent will not have dimensions, so the children can't expand.
+      // What if you add `height: 300` instead of `flex: 1`?
+      <View style={{
+      		flex:1,
+      		flexDirection: 'row',
+      		justifyContent: 'flex-start',
+//      		alignItems: 'center',
+      }}>
+        <View style={{height: 50, width: 50, backgroundColor: 'powderblue'}} />
+        <View style={{height: 50, width: 50, backgroundColor: 'skyblue'}} />
+        <View style={{height: 50, width: 50, backgroundColor: 'steelblue'}} />
+      </View>
+    );
+  }
+};
+
+class PizzaTranslator extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {text: ''};
+  }
+  render(){
+  	return (
+  		<View style={{padding: 20}}>
+  			<TextInput
+  				style={{height: 40}}
+  				placeholder="what fuck u say"
+  				onChangeText={(text) => this.setState({text})}
+  			/>
+				<Text style={{padding: 20, frontSize: 40}}>
+					{this.state.text.split(' ').map((word) => 'sdwac').join(' ')}
+				</Text>
+  		</View>
+  	)
+  }
+ }
+
+class ListViewBasics extends Component {
+  // Initialize the hardcoded data
+  constructor(props) {
+    super(props);
+    const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+    this.state = {
+      dataSource: ds.cloneWithRows([
+        'John', 'Joel', 'ssss', 'Jimmy', 'Jackson', 'Jillian', 'Julie', 'Devin'
+      ])
+    };
+  }
+
+  render() {
+    return (
+      <View style={{paddingTop: 22}}>
+        <ListView
+          dataSource={this.state.dataSource}
+          renderRow={(rowData) => <Text>{rowData}</Text>}
+        />
+//        <Text>{this.getMoviesFromApiAsync()}</Text>
+      </View>
+    );
+  }
+}
+
+class YoDawgApp extends Component {
+  render() {
+    return (
+   		<Navigator
+          initialRoute={{ title: 'My Initial Scene', index: 0 }}
+          renderScene={(route, navigator) => {
+            return (
+							<MyScene
+								title={route.title}
+
+								// Function to call when a new scene should be displayed
+                onForward = {() => {
+                	const nextIndex = route.index + 1;
+                	navigator.push({
+                		title: 'Scene' + nextIndex,
+                		index: nextIndex,
+                	});
+                }}
+                // Function to call to go back to the previous scene
+                onBack={() => {
+                	if (route.index > 0) {
+                		navigator.pop();
+                	}
+                }}
+							/>
+            )
+          }}
+       />
+//      <MyScene />
+    )
   }
 }
 
@@ -84,4 +190,6 @@ const styles = StyleSheet.create({
   },
 });
 
-AppRegistry.registerComponent('AProject', () => AProject);
+
+
+AppRegistry.registerComponent('AProject', () => YoDawgApp);
